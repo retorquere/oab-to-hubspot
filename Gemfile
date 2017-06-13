18eb757da728bb93b120887267efa30faee6346e
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2017-06-13 22:45:53 +0200

    first commit

---
+++ Gemfile
@@ -0,0 +1,8 @@
+#ruby=2.3
+#ruby-gemset=oab
+
+source 'https://rubygems.org'
+
+gem 'exchange-offline-address-book'
+gem 'phony'
+gem "hubspot-ruby"
---
+++ oab2hubspot.rb
@@ -0,0 +1,102 @@
+#!/usr/bin/env ruby
+
+require 'dotenv/load'
+require 'exchange-offline-address-book'
+require 'phony'
+require 'ostruct'
+require 'namae'
+require 'hubspot-ruby'
+
+Hubspot.configure(hapikey: ENV["HUBSPOT"])
+
+oab = OfflineAddressBook.new(username: ENV['USERNAME'], password: ENV['PASSWORD'], email: ENV['EMAIL'], cachedir: File.dirname(__FILE__))
+
+GAL = {}
+
+def normalize(number)
+  number = "#{number}".strip
+  number = "00#{number}" if number =~ /^31/
+  number.gsub!(/^\+/, '00')
+  number.gsub!(/^0([1-9])/) { "0031#{$1}" }
+  number.gsub!(/^00/, '+')
+  Phony.plausible?(number) ? Phony.format(Phony.normalize(number), format: :international) : nil
+end
+
+PRIMARY = []
+oab.records.each{|record|
+  next unless record.SmtpAddress
+
+  contact = {
+    lastname: record.DisplayName,
+    email: record.SmtpAddress.downcase,
+    primary: [],
+    secondary: [],
+  }
+
+
+  [
+    :BusinessTelephoneNumber,
+    :Business2TelephoneNumber,
+    :MobileTelephoneNumber,
+    :Assistant,
+    :AssistantTelephoneNumber,
+  ].each{|field|
+    next unless record[field]
+
+    kind = field.to_s =~ /Assistant/ ? :secondary : :primary
+
+    if record[field].is_a?(String)
+      contact[kind] << normalize(record[field])
+    else
+      contact[kind].concat(record[field].collect{|n| normalize(n)})
+    end
+  }
+  contact[:primary].compact!
+  contact[:primary].uniq!
+  contact[:secondary].compact!
+  contact[:secondary].uniq!
+  PRIMARY.concat(contact[:primary])
+
+  next if contact[:primary].empty? && contact[:secondary].empty?
+
+  contact[:lastname] = "#{$2} #{$1}".strip if contact[:lastname] =~ /^(#{record.Surname}[^ ]*) (.*)/
+  raise "duplicate email address #{contact[:email]}" if GAL[contact[:email]]
+  GAL[contact[:email]] = contact
+}
+
+GAL.values.each{|contact|
+  contact[:secondary] = contact[:secondary] - PRIMARY
+  contact[:primary].each{|n|
+    parts = Phony.split(Phony.normalize(n))
+    if parts[0,2] == ['31', '6'] && !contact[:mobilephone]
+      contact[:mobilephone] = n
+    elsif parts[0,2] != ['31', '6'] && !contact[:phone]
+      contact[:phone] = n
+    elsif !contact[:phone]
+      contact[:phone] = n
+    elsif !contact[:mobilephone]
+      contact[:mobilephone] = n
+    else
+      raise contact[:lastname]
+    end
+  }
+  contact[:secondary].each{|n|
+    parts = Phony.split(Phony.normalize(n))
+    if parts[0,2] == ['31', '6'] && !contact[:mobilephone]
+      contact[:mobilephone] = n
+    elsif parts[0,2] != ['31', '6'] && !contact[:phone]
+      contact[:phone] = n
+    elsif !contact[:secondary_phone]
+      contact[:secondary_phone]= n
+    else
+      raise contact[:lastname]
+    end
+  }
+
+  contact.delete(:primary)
+  contact.delete(:secondary)
+}
+
+GAL.values.each_slice(100).each{|contacts|
+  Hubspot::Contact.create_or_update!(contacts)
+}
